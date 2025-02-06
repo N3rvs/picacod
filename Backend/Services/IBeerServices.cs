11868d93bd591732f708138094c82e781f84adc4
guillermo longueira lopez <longueiraspain@gmail.com>
2025-02-06 13:34:56 +0100

    Refactorizacion

---
+++ Backend/Services/IBeerServices.cs
@@ -0,0 +1,14 @@
+﻿using Backend.DTOs;
+
+namespace Backend.Services
+{
+    public interface IBeerServices
+    {
+        Task<IEnumerable<BeerDto>> Get();
+        Task<BeerDto> GetById(int id);
+        Task<BeerDto> Add(BeerDto beerInsertDto);
+        Task <BeerDto> Update(int id,BeerDto beerUpdateDto);
+        Task<BeerDto> Delete(int id);
+
+    }
+}
